503ca0ec2d7c2e15eab3cfc59d2b2ad49ad68b99
admin-supreme <ayushroy18102010@gmail.com>
2026-03-12 22:59:19 +0530

    Refactor response handling in worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,5 @@
-import Home from './src/home.js'
-import Search from './src/search.js'
-import Anime from './src/anime.js'
+import Home from "./src/home.js"
+import Search from "./src/search.js"
 
 export default {
   async fetch(request) {
@@ -10,15 +9,20 @@ export default {
 
     if (path === "/latest") {
       const data = await Home.latest_series()
-      return Response.json(data)
+      return new Response(JSON.stringify(data), {
+        headers: { "content-type": "application/json" }
+      })
     }
 
     if (path === "/search") {
       const q = url.searchParams.get("q")
       const data = await Search.default_search(q)
-      return Response.json(data)
+
+      return new Response(JSON.stringify(data), {
+        headers: { "content-type": "application/json" }
+      })
     }
 
-    return new Response("Worker running")
+    return new Response("API Running")
   }
 }
