475692715eecffa431ef6ea8a180be1961d6e421
admin-supreme <ayushroy18102010@gmail.com>
2026-03-12 22:41:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Home = require('./src/home');
+const Category = require('./src/category');
+const Search = require('./src/search');
+const Anime = require('./src/anime');
+
+module.exports = {
+    Home,
+    Category,
+    Search,
+    Anime
+}
